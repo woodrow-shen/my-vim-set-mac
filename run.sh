ff4995740d0948d524a02ff4e3d9884e5421b2ab
Woodrow Shen <woodrow.shen@gmail.com>
2016-04-03 16:57:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install vim
+brew isntall vim
+
+# install go
+brew install go
+
+cat << EOF >> $HOME/.bash_profile
+export GOROOT=/usr/local/opt/go/libexec
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+EOF
+
+source $HOME/.bash_profile
 
 # install ycm
 mkdir -p $HOME/.vim/bundle
